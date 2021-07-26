ed8165a41ef3b6f7ea4998ed1ec82d79c7d451aa
TinaLand <huihuijiang0619@gmail.com>
2021-07-26 16:44:12 -0400

    Added network configuaration

--- app/src/main/java/com/example/news/MainActivity.java
+++ app/src/main/java/com/example/news/MainActivity.java
@@ -1,13 +1,21 @@
 package com.example.news;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
 
+import com.example.news.model.NewsResponse;
+import com.example.news.network.NewsApi;
+import com.example.news.network.RetrofitClient;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +31,23 @@ public class MainActivity extends AppCompatActivity {
        navController = navHostFragment.getNavController();
        NavigationUI.setupWithNavController(navView, navController);
        NavigationUI.setupActionBarWithNavController(this, navController);
+
+       NewsApi api = RetrofitClient.newInstance(this).create(NewsApi.class);
+
+       api.getTopHeadlines("US").enqueue(new Callback<NewsResponse>() {
+           @Override
+           public void onResponse(Call<NewsResponse> call, Response<NewsResponse> response) {
+               if (response.isSuccessful()) {
+                       Log.d("getTopHeadlines", response.body().toString());
+                   } else {
+                       Log.d("getTopHeadlines", response.toString());
+                   }
+               }
+        @Override
+        public void onFailure(Call<NewsResponse> call, Throwable t) {
+               Log.d("getTopHeadlines", t.toString());
+           }
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/example/news/TinNewsApplication.java
@@ -0,0 +1,11 @@
+package com.example.news;
+
+import android.app.Application;
+
+public class TinNewsApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // TODO: new code here.
+    }
+}
