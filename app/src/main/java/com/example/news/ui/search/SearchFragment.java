888d368109d4818a237d28b0978926860583b24f
TinaLand <huihuijiang0619@gmail.com>
2021-07-26 20:50:58 -0400

    Update

--- app/src/main/java/com/example/news/ui/search/SearchFragment.java
+++ app/src/main/java/com/example/news/ui/search/SearchFragment.java
@@ -2,15 +2,17 @@ package com.example.news.ui.search;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.SearchView;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
 
-import com.example.news.repository.NewsRepository;
-import com.example.news.repository.NewsViewModelFactory;
+import com.example.news.databinding.FragmentSearchBinding;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -20,17 +22,41 @@ import com.example.news.repository.NewsViewModelFactory;
 public class SearchFragment extends Fragment {
 
     private SearchViewModel viewModel;
+    private FragmentSearchBinding binding;
 
-    // ... existing code
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+      binding = FragmentSearchBinding.inflate(inflater, container, false);
+      return binding.getRoot();
+    }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        NewsRepository repository = new NewsRepository(getContext());
-        viewModel = new ViewModelProvider(this, new NewsViewModelFactory(repository))
-                .get(SearchViewModel.class);
-        viewModel.setSearchInput("Covid-19");
+        SearchNewsAdapter newsAdapter = new SearchNewsAdapter();
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(requireContext(), 2);
+        binding.newsResultsRecyclerView.setLayoutManager(gridLayoutManager);
+        binding.newsResultsRecyclerView.setAdapter(newsAdapter);
+
+        binding.newsSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                if (!query.isEmpty()) {
+                    viewModel.setSearchInput(query);
+                }
+                binding.newsSearchView.clearFocus();
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+
         viewModel
                 .searchNews()
                 .observe(
@@ -38,6 +64,7 @@ public class SearchFragment extends Fragment {
                         newsResponse -> {
                             if (newsResponse != null) {
                                 Log.d("SearchFragment", newsResponse.toString());
+                                                      newsAdapter.setArticles(newsResponse.articles);
                             }
                         });
     }
